8e5697215b66ef3ea7e0cf5d8b5064d6e7387b76
baekcii <bsy8470@naver.com>
2026-03-06 18:20:17 +0900

    chore: 코드 스타일 수정

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/CardData.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/CardData.kt
@@ -2,7 +2,7 @@ package woowacourse.kanban.board
 
 data class CardData(
     val title: String,
-    val content: String?=null,
+    val content: String? = null,
     val chips: List<String> = emptyList(),
     val user: UserData,
 )

e89619f30edd06b92a49a02506900c85ce906ecf
baekcii <bsy8470@naver.com>
2026-03-09 13:11:16 +0900

    refac: 컴포넌트 및 데이터 모델 이름 변경

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/UserData.kt
+++
@@ -1,3 +0,0 @@
-package woowacourse.kanban.board
-
-data class UserData(val name: String, val profileImg: String? = null)
---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/UserInfo.kt
@@ -0,0 +1,3 @@
+package woowacourse.kanban.board
+
+data class UserInfo(val name: String, val profileImg: String? = null)

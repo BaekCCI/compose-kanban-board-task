7bf47ca5d5135925c5a4c9400040a740785d59ac
baekcii <bsy8470@naver.com>
2026-03-09 13:28:59 +0900

    feat: UserInfo 이름 공백 검증 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/UserInfo.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/UserInfo.kt
@@ -1,3 +1,9 @@
 package woowacourse.kanban.board
 
-data class UserInfo(val name: String, val profileImg: String? = null)
+data class UserInfo(val name: String, val profileImg: String? = null) {
+    init {
+        require(name.isNotBlank()) {
+            "이름은 공백일 수 없습니다."
+        }
+    }
+}

78d9309824c1571597d303872dc82a65dc23755d
baekcii <bsy8470@naver.com>
2026-03-09 15:42:30 +0900

    feat: 태그 데이터 모델 정의

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/Tag.kt
@@ -0,0 +1,15 @@
+package woowacourse.kanban.board.model
+
+
+@JvmInline
+value class Tag(
+    val content: String,
+) {
+    init {
+        require(content.length in 1..MAX_TAG_LENGTH) { "최대 $MAX_TAG_LENGTH 글자까지 입력 가능합니다." }
+    }
+
+    companion object {
+        const val MAX_TAG_LENGTH = 5
+    }
+}

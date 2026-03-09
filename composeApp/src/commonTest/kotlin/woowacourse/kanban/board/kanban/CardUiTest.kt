ab7e7b8e8ed619559b0c3c0879ee27558b2793f1
baekcii <bsy8470@naver.com>
2026-03-09 17:20:37 +0900

    chore: 수업 내용 분리 및 import 정리

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/kanban/CardUiTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/kanban/CardUiTest.kt
@@ -4,11 +4,11 @@ import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.runComposeUiTest
-import kotlin.test.Test
 import woowacourse.kanban.board.KanbanCard
 import woowacourse.kanban.board.model.Card
 import woowacourse.kanban.board.model.Tag
 import woowacourse.kanban.board.model.UserInfo
+import kotlin.test.Test
 
 @OptIn(ExperimentalTestApi::class)
 class CardUiTest {

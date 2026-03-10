d9c7e7c62ffd70d3e1e41fe12272268ac6d1a347
baekcii <bsy8470@naver.com>
2026-03-10 17:24:03 +0900

    refac: 태그 글자 수 최솟값 상수화

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/Tag.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/Tag.kt
@@ -3,10 +3,11 @@ package woowacourse.kanban.board.model
 @JvmInline
 value class Tag(val content: String) {
     init {
-        require(content.length in 1..MAX_TAG_LENGTH) { "최대 $MAX_TAG_LENGTH 글자까지 입력 가능합니다." }
+        require(content.length in MIN_TAG_LENGTH..MAX_TAG_LENGTH) { "최대 $MAX_TAG_LENGTH 글자까지 입력 가능합니다." }
     }
 
     companion object {
+        const val MIN_TAG_LENGTH = 1
         const val MAX_TAG_LENGTH = 5
     }
 }
